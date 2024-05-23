d56345b0378892ed23a9349ffd3f46a1fd60c3b2
agilardino <alejandro.gilardino@gmail.com>
2024-05-23 16:57:52 -0500

    owl
    se suma owl

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+ 
+ // NAVEGACION
+
+ const nav = document.querySelector("#nav");
+ const abrir = document.querySelector("#abrir");
+ const cerrar = document.querySelector("#cerrar");
+ 
+ abrir.addEventListener("click", () => {
+     nav.classList.add("visible");
+ })
+ 
+ cerrar.addEventListener("click", () => {
+     nav.classList.remove("visible");
+ })
+
+// owl carousel
+
+
+
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
     'use strict'
@@ -18,17 +38,3 @@
         }, false)
       })
   })()
-
-  // NAVEGACION
-
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
-
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
-
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
